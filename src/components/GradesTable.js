bbb5c08def2e720f67b7f5b1ed9cd84034027274
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2023-04-01 11:54:26 +0530

    grades list ui

--- src/components/GradesTable.js
+++ src/components/GradesTable.js
@@ -1,5 +1,6 @@
 import { Table } from "@themesberg/react-bootstrap";
 import React from "react";
+import grades from "../utilities/gradesList";
 
 function GradesTable() {
   return (
@@ -14,18 +15,30 @@ function GradesTable() {
           <th>Revised Count</th>
         </tr>
       </thead>
-      <tbody></tbody>
+      <tbody>
+        {grades.map((grade, index) => (
+          <Element e={grade} index={index} />
+        ))}
+      </tbody>
     </Table>
   );
 }
 
 export default GradesTable;
 
-function Element({ val }) {
+function Element({ e, index }) {
   return (
-    <tr>
-      <td>{val}</td>
-      <td>{val}</td>
+    <tr key={index}>
+      <td>{e.grade}</td>
+      <td>
+        {e.starts_at} - {e.ends_at}
+      </td>
+      <td>{e.count}</td>
+      <td>0</td>
+      <td>
+        {e.starts_at} - {e.ends_at}
+      </td>
+      <td>{e.count}</td>
     </tr>
   );
 }
---
+++ src/utilities/gradesList.js
@@ -0,0 +1,52 @@
+const grades = [
+  {
+    grade: "O",
+    starts_at: 91,
+    ends_at: 100,
+    count: 7,
+  },
+  {
+    grade: "A+",
+    starts_at: 82,
+    ends_at: 90,
+    count: 16,
+  },
+  {
+    grade: "A",
+    starts_at: 73,
+    ends_at: 81,
+    count: 5,
+  },
+  {
+    grade: "B+",
+    starts_at: 64,
+    ends_at: 72,
+    count: 12,
+  },
+  {
+    grade: "B",
+    starts_at: 55,
+    ends_at: 63,
+    count: 19,
+  },
+  {
+    grade: "C",
+    starts_at: 46,
+    ends_at: 54,
+    count: 8,
+  },
+  {
+    grade: "P",
+    starts_at: 35,
+    ends_at: 45,
+    count: 3,
+  },
+  {
+    grade: "F",
+    starts_at: 0,
+    ends_at: 34,
+    count: 7,
+  },
+];
+
+export default grades;
